f756d44a6e755c3165a2f81cfee3bec1944f93e4
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2018-01-20 10:16:17 -0800

    finish first attempt at 2D array

--- lab-shannon/model/2D-array.js
+++ lab-shannon/model/2D-array.js
@@ -1,4 +1,4 @@
-let convertHeightToInches(person){
+let _convertHeightToInches(person){
   let height = person.split(' ');
   if(height.length === 4){
     heightInInches = (height[0] * 12) + height[2];
@@ -6,4 +6,21 @@ let convertHeightToInches(person){
     heightInInches = height[0];
   }
   return heightInInches;
+}
+
+let findBlocking(theatre, row, seat){
+  let blocking = 0;
+  let column = [];
+  for(let i = 0; i < theatre.length; i++){
+    column.push(theatre[i][seat]);
+  }
+  for(let i = 0; i < column.length; i++){
+    column[i] = _convertHeightToInches(column[i]);
+  }
+  for(let i = row + 1; i < column.length; i++){
+    if(column[row] > column[i]){
+      blocking++;
+    }
+  }
+  return blocking;
 }
