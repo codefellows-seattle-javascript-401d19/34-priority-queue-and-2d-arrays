868504d0f2e4b04c7ce3d222c31e97fcd1085301
Cameron Moorehead <hello@cameronmoorehead.com>
2018-01-21 11:11:08 -0800

    theater finished

--- lab-cameron/src/min-heap.js
+++ lab-cameron/src/min-heap.js
@@ -1,76 +1,82 @@
 'use strict';
 
 class MinHeap{
-  constructor(){
+  constructor() {
     this._data = [];
   }
 
-  _getParentIndex(index){
-    if(index === 0)
+  _getParentIndex(index) {
+    if (index === 0) {
       return null;
+    }
 
     return Math.floor((index - 1) / 2);
   }
 
-  _getLeftIndex(index){
+  _getLeftIndex(index) {
     return (2 * index) + 1;
   }
 
-  _getRightIndex(index){
+  _getRightIndex(index) {
     return (2 * index) + 2;
   }
 
-  // lg n
-  insert(value){
-    if(typeof value !== 'number')
-      throw new TypeError('__ERROR__ value should be numeric');
+  insert(item) {
+    if (typeof item !== 'object') {
+      throw new TypeError('__ERROR__ item should be an object');
+    }
 
-    this._data.push(value);
+    this._data.push(item);
     this._bubbleUp(this._data.length - 1);
   }
 
-  _swapValues(indexA,indexB){
+  _swapValues(indexA,indexB) {
     let tempSwapValue = this._data[indexA];
 
     this._data[indexA] = this._data[indexB];
     this._data[indexB] = tempSwapValue;
   }
 
-  _bubbleUp(index){
-    if(this._getParentIndex(index) === null)
+  _bubbleUp(index) {
+    if (this._getParentIndex(index) === null) {
       return;
+    }
 
     let parentIndex = this._getParentIndex(index);
 
-    if(this._data[parentIndex] > this._data[index]){
+    if (this._data[parentIndex].priority > this._data[index].priority) {
       this._swapValues(parentIndex, index);
       this._bubbleUp(parentIndex);
     }
   }
 
-  _bubbleDown(index){
+  _bubbleDown(index) {
     let minIndex = index;
     let leftIndex = this._getLeftIndex(index);
     let rightIndex = this._getRightIndex(index);
 
-    if(leftIndex <= this._data.length - 1){
-      if(this._data[minIndex] > this._data[leftIndex])
+    if (leftIndex <= this._data.length - 1) {
+      if (this._data[minIndex].priority > this._data[leftIndex].priority) {
         minIndex = leftIndex;
-    }
-    if(rightIndex <= this._data.length - 1){
-      if(this._data[minIndex] > this._data[rightIndex])
-        minIndex = rightIndex;
+      }
     }
 
-    if(minIndex != index){
+    if (rightIndex <= this._data.length - 1) {
+      if (this._data[minIndex].priority > this._data[rightIndex].priority) {
+        minIndex = rightIndex;
+      }
+    }
+
+    if (minIndex != index) {
       this._swapValues(index, minIndex);
       this._bubbleDown(minIndex);
     }
   }
 
-  extractMininum(){
-    if(this._data.length <= 0)
+  extractMininum() {
+    if (this._data.length <= 0) {
       return null;
+    }
 
     let min = this._data[0];
     let lastValue = this._data.pop();
@@ -79,11 +85,16 @@ class MinHeap{
     return min;
   }
 
-  peek(){
-    if(this._data.length <= 0)
+  peek() {
+    if (this._data.length <= 0) {
       return null;
+    }
 
     return this._data[0];
+  }
+
+  isEmpty() {
+    return this._data.length === 0;
   }
 }
 
--- lab-cameron/src/priority-queue.js
+++ lab-cameron/src/priority-queue.js
@@ -1,1 +1,48 @@
 const MinHeap = require('./min-heap');
+
+const PriorityQueue1 = function() {
+  const minHeap = new MinHeap();
+
+  this.enqueue = item => {
+    minHeap.insert(item);
+  };
+
+  this.dequeue = () => {
+    return minHeap.extractMininum();
+  };
+
+  this.peek = () => {
+    return minHeap.peek();
+  };
+
+  this.isEmpty = () => {
+    return minHeap.isEmpty();
+  };
+};
+
+const PriorityQueue2 = function() {
+  const items = [];
+
+  this.enqueue = item => {
+    for (let i = 0; i < items.length; i++) {
+      if (item.priority < items[i]) {
+        items.splice(i, 0, item);
+        return;
+      }
+    }
+
+    items.push(item);
+  };
+
+  this.dequeue = () => {
+    return items.shift();
+  };
+
+  this.peek = () => {
+    return items[0];
+  };
+
+  this.isEmpty = () => {
+    return items.length === 0;
+  };
+};
