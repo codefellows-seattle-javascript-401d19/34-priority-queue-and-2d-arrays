fb82a4a5254fcedfbfd30fea69de8532c7db78ab
snobeard <fatmattmatt@gmail.com>
2018-01-22 07:06:10 -0800

    all tests pass

---
+++ lab-matt/src/__test__/priority-queue.test.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const PriorityQueue = require('../model/priority-queue');
+
+let myQueue = new PriorityQueue;
+
+myQueue.enqueue({ priority: 3, value: 'three'});
+myQueue.enqueue({ priority: 1, value: 'one'});
+myQueue.enqueue({ priority: 6, value: 'six'});
+myQueue.enqueue({ priority: 9, value: 'nine'});
+myQueue.enqueue({ priority: 4, value: 'four'});
+myQueue.enqueue({ priority: 2, value: 'two'});
+myQueue.enqueue({ priority: 5, value: 'five'});
+myQueue.enqueue({ priority: 0, value: 'zero'});
+myQueue.enqueue({ priority: 7, value: 'seven'});
+myQueue.enqueue({ priority: -100, value: '-100'});
+myQueue.enqueue({ priority: 8, value: 'eight'});
+myQueue.enqueue({ priority: 100, value: '100'});
+
+test('peek returns the lowest priority value but doesn\t remove it', () => {
+  expect(myQueue.peek()).toEqual({ priority: -100, value: '-100'});
+  expect(myQueue.peek()).toEqual({ priority: -100, value: '-100'});
+  expect(myQueue.peek()).toEqual({ priority: -100, value: '-100'});
+});
+test('priority queue focuses on lowest number as highest priority', () => {
+  expect(myQueue.dequeue().priority).toEqual(-100);
+  expect(myQueue.dequeue().priority).toEqual(0);
+  expect(myQueue.dequeue().priority).toEqual(1);
+  expect(myQueue.dequeue().priority).toEqual(2);
+  expect(myQueue.dequeue().priority).toEqual(3);
+  expect(myQueue.dequeue().priority).toEqual(4);
+  expect(myQueue.dequeue().priority).toEqual(5);
+  expect(myQueue.dequeue().priority).toEqual(6);
+  expect(myQueue.dequeue().priority).toEqual(7);
+  expect(myQueue.dequeue().priority).toEqual(8);
+  expect(myQueue.dequeue().priority).toEqual(9);
+  expect(myQueue.dequeue().priority).toEqual(100);
+});
+test('expect an empty priority queue to return null with .dequeue()', () => {
+  expect(myQueue.dequeue()).toEqual(null);
+});
+test('expect an empty priority queue to return null with peek()', () => {
+  expect(myQueue.peek()).toEqual(null);
+});
+test('expect an error if the object doesn\'t have a priority or value', () => {
+  expect(() => {
+    myQueue.enqueue({ priority: 0});
+  }).toThrow();
+  expect(() => {
+    myQueue.enqueue({ value: 'zero'});
+  }).toThrow();
+});
---
+++ lab-matt/src/model/priority-queue.js
@@ -0,0 +1,93 @@
+'use strict';
+
+class PriorityQueue {
+  constructor() {
+    this._data = [];
+  }
+
+  _getParentIndex(index) {
+    if (index === 0)
+      return null;
+
+    return Math.floor((index -1) / 2);
+  }
+
+  _getLeftIndex(index) {
+    return (2 * index) + 1;
+  }
+
+  _getRightIndex(index) {
+    return (2 * index) + 2;
+  }
+
+  enqueue(object) {
+    if (object.value === undefined)
+      throw new TypeError('__ERROR__ object needs a value');
+    if (object.priority === undefined)
+      throw new TypeError('__ERROR__ priority needs to be a number');
+      
+
+      
+    this._data.push(object);
+    this._bubbleUp(this._data.length -1);
+  }
+
+  _swapValues(indexA,indexB) {
+    let tempSwapObject = this._data[indexA];
+
+    this._data[indexA] = this._data[indexB];
+    this._data[indexB] = tempSwapObject;
+  }
+
+  _bubbleUp(index) {
+    if (this._getParentIndex(index) === null)
+      return;
+
+    let parentIndex = this._getParentIndex(index);
+
+    if (this._data[parentIndex].priority > this._data[index].priority) {
+      this._swapValues(parentIndex, index);
+      this._bubbleUp(parentIndex);
+    } 
+  }
+
+  _bubbleDown(index) {
+    let minIndex = index;
+    let leftIndex = this._getLeftIndex(index);
+    let rightIndex = this._getRightIndex(index);
+
+    if (leftIndex <= this._data.length -1) {
+      if (this._data[minIndex].priority > this._data[leftIndex].priority)
+        minIndex = leftIndex;
+    }
+    if (rightIndex <= this._data.length -1) {
+      if (this._data[minIndex].priority > this._data[rightIndex].priority)
+        minIndex = rightIndex;
+    }
+
+    if (minIndex != index) {
+      this._swapValues(index, minIndex);
+      this._bubbleDown(minIndex);
+    }
+  }
+
+  dequeue() {
+    if (this._data.length <= 1)
+      return this._data.pop() || null;
+    
+    let maxPriority = this._data[0];
+    let lastValue = this._data.pop();
+    this._data[0] = lastValue;
+    this._bubbleDown(0);
+    return maxPriority;
+  }
+
+  peek() {
+    if (this._data.length <= 0)
+      return null;
+    
+    return this._data[0];
+  }
+}
+
+module.exports = PriorityQueue;
