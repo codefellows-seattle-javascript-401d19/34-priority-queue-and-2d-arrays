1c6134cf31a387743ba2122aca4af94a27277cb0
Catherine Looper <loopercat@gmail.com>
2018-01-21 17:39:14 -0800

    Added tests for 2d array Type Error handling

--- lab-catherine/__test__/2d-array.test.js
+++ lab-catherine/__test__/2d-array.test.js
@@ -3,6 +3,44 @@
 const MovieTheater = require('../src/2d-array');
 
 describe('2D Array - Movie Theater', () => {
+  test('Movie Theater should throw an error if theater is not an array', () => {
+    let theater = 'notAnArray';
+    let movieTheater = new MovieTheater();
+    expect(() => {
+      movieTheater(2, 3, theater);
+    }).toThrow();
+  });
+
+  test('Movie Theater should throw an error if seat is not a number', () => {
+    let theater = [
+      [ '1 feet 11 inches', '4 feet 8 inches', '2 feet 10 inches'], 
+      ['3 feet 5 inches', '2 inches', '3 inches', '11 inches' ],
+      [ '2 feet 10 inches', '11 inches', '7 inches'],
+      ['2 feet 9 inches', '0 inches', '9 inches'],
+      [ '3 inches', '1 inches', '5 feet 5 inches'],
+      ['6 inches', '2 inches', '5 feet 7 inches'],
+    ];
+    let movieTheater = new MovieTheater();
+    expect(() => {
+      movieTheater('notANumber', 3, theater);
+    }).toThrow();
+  });
+
+  test('Movie Theater should throw an error if row is not a number', () => {
+    let theater = [
+      [ '1 feet 11 inches', '4 feet 8 inches', '2 feet 10 inches'], 
+      ['3 feet 5 inches', '2 inches', '3 inches', '11 inches' ],
+      [ '2 feet 10 inches', '11 inches', '7 inches'],
+      ['2 feet 9 inches', '0 inches', '9 inches'],
+      [ '3 inches', '1 inches', '5 feet 5 inches'],
+      ['6 inches', '2 inches', '5 feet 7 inches'],
+    ];
+    let movieTheater = new MovieTheater();
+    expect(() => {
+      movieTheater(2, 'notANumber', theater);
+    }).toThrow();
+  });
+
   test('Movie Theater should return the number of blocked people', () => {
     let theater = [
       [ '1 feet 11 inches', '4 feet 8 inches', '2 feet 10 inches'], 
