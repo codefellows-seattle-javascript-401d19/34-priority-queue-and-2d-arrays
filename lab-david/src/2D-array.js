35fda5659e51502bc4619f7cbe469d7709b7a4a6
David Lindahl <davidalindahl@gmail.com>
2018-01-22 08:50:35 -0800

    added some psuedo code to get stuff figured out

--- lab-david/src/2D-array.js
+++ lab-david/src/2D-array.js
@@ -1,2 +1,25 @@
 'use strict';
 
+class Theatre{
+  constructor(row, seat){
+    // check to make sure theatre is an array
+
+    // check to make sure things are integers
+
+    // make sure positive numbers
+
+    // run for loop over theatre column at row #
+    // add counter each time
+    // return counter
+
+  }
+}
+
+_getInches(heightNumber){
+  let height = heightNumber.split(' ');
+  let heightInInches;
+  if(height.length < 2)
+    return height[0]
+  else 
+    return ( (height[0] x 12) + height[2] );
+  }
