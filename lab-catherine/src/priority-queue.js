3e1e027dd1f892715201a5d07d66cf8ae47139ed
Catherine Looper <loopercat@gmail.com>
2018-01-21 18:24:53 -0800

    Scaffolded priority queue build

--- lab-catherine/src/priority-queue.js
+++ lab-catherine/src/priority-queue.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const MinHeap = require('./min-heap');
+
+class PriorityQueue {
+  constructor() {
+    this._queue = null;
+    this._data = [];
+  }
+
+  enqueue(value) {
+    this._data.push(value);
+  }
+
+  dequeue() {
+    return this._data.shift();
+  }
+}
+
+module.exports = PriorityQueue;
