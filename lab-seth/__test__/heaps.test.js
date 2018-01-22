5d715257b5a32f7653a391c3d7d78d0db646bd0e
SethDonohue <donohue.seth@gmail.com>
2018-01-21 16:10:18 -0800

    all tests GREEN for Priority Queue, 100% Coverage

---
+++ lab-seth/__test__/heaps.test.js
@@ -0,0 +1,97 @@
+const PriorityQueue = require('../src/priority-queue');
+
+const mockPriorityFactory = () => {
+  let mockPriorityQueue = new PriorityQueue();
+  mockPriorityQueue.enqueue({ priority: 1, value: 'testValue1' });
+  mockPriorityQueue.enqueue({ priority: 10, value: 'testValue10' });
+  mockPriorityQueue.enqueue({ priority: 3, value: 'testValue3' });
+  mockPriorityQueue.enqueue({ priority: 4, value: 'testValue4' });
+  mockPriorityQueue.enqueue({ priority: 2, value: 'testValue2' });
+  mockPriorityQueue.enqueue({ priority: 1, value: 'testValue1A' });
+
+  return mockPriorityQueue;
+}
+
+describe('Priority Queue', () => {
+  test('Proper creation of Priority Queue class and one insertion()', () => {
+    let mockPriorityQueue = new PriorityQueue();
+    let mockPriorityObject = {priority : 1, value: 'testValue'};
+    expect(mockPriorityQueue._data).toEqual([]);
+
+    mockPriorityQueue.enqueue(mockPriorityObject);
+    expect(mockPriorityQueue._data).toEqual([{ priority: 1, value: 'testValue' }]);
+  });
+
+  test('Proper insertion of a single Priority Object, multiple times', () => {
+    let mockPriorityQueue = new PriorityQueue();
+
+    mockPriorityQueue.enqueue({ priority: 1, value: 'testValue1' });
+    mockPriorityQueue.enqueue({ priority: 10, value: 'testValue10' });
+    mockPriorityQueue.enqueue({ priority: 3, value: 'testValue3' });
+    mockPriorityQueue.enqueue({ priority: 4, value: 'testValue4' });
+    mockPriorityQueue.enqueue({ priority: 2, value: 'testValue2' });
+    mockPriorityQueue.enqueue({ priority: 1, value: 'testValue1A' });
+
+    expect(mockPriorityQueue._data[0].priority).toEqual(1);
+    expect(mockPriorityQueue._data[1].priority).toEqual(2);
+    expect(mockPriorityQueue._data[2].priority).toEqual(1);
+    expect(mockPriorityQueue._data[3].priority).toEqual(10);
+    expect(mockPriorityQueue._data[4].priority).toEqual(4);
+    expect(mockPriorityQueue._data[5].priority).toEqual(3);
+  });
+
+  test('Method Enqueue() should throw error if priority is not numeric', () => {
+    const mockPriorityQueue = new PriorityQueue();
+
+    expect(() => {
+      mockPriorityQueue.enqueue('4');
+    }).toThrow();
+
+    expect(() => {
+      mockPriorityQueue.enqueue({priority: '4', value: 'falseTest4'});
+    }).toThrow();
+  });
+
+  test('Dequeue() should return the first Priority Object of the max-heap and modify the array so it is still a max-heap', () => {
+    let mockPriorityQueue = mockPriorityFactory();
+    expect(mockPriorityQueue._data[0].priority).toEqual(1);
+    expect(mockPriorityQueue._data[1].priority).toEqual(2);
+    expect(mockPriorityQueue._data[2].priority).toEqual(1);
+    expect(mockPriorityQueue._data[3].priority).toEqual(10);
+    expect(mockPriorityQueue._data[4].priority).toEqual(4);
+    expect(mockPriorityQueue._data[5].priority).toEqual(3);
+
+
+    expect(mockPriorityQueue.dequeue()).toEqual('testValue1');
+
+    expect(mockPriorityQueue._data[0].priority).toEqual(1);
+    expect(mockPriorityQueue._data[1].priority).toEqual(2);
+    expect(mockPriorityQueue._data[2].priority).toEqual(3);
+    expect(mockPriorityQueue._data[3].priority).toEqual(10);
+    expect(mockPriorityQueue._data[4].priority).toEqual(4);    
+  });
+
+  test('Method Dequeue() should return null if Priority Queue is empty', () => {
+    const mockPriorityQueue = new PriorityQueue();
+
+    expect(mockPriorityQueue.dequeue()).toEqual(null);
+  });
+
+  test('Method Peek() should return Highest Priority Object which is also the root value', () => {
+    let mockPriorityQueue = mockPriorityFactory();
+    expect(mockPriorityQueue._data[0].priority).toEqual(1);
+    expect(mockPriorityQueue._data[1].priority).toEqual(2);
+    expect(mockPriorityQueue._data[2].priority).toEqual(1);
+    expect(mockPriorityQueue._data[3].priority).toEqual(10);
+    expect(mockPriorityQueue._data[4].priority).toEqual(4);
+    expect(mockPriorityQueue._data[5].priority).toEqual(3);
+
+    expect(mockPriorityQueue.peek()).toEqual({priority: 1, value: 'testValue1'});
+  });
+
+  test('Method Peek() should return null if Priority Queue is empty', () => {
+    const mockPriorityQueue = new PriorityQueue();
+
+    expect(mockPriorityQueue.peek()).toEqual(null);
+  });
+});
--- lab-seth/src/priority-queue.js
+++ lab-seth/src/priority-queue.js
@@ -1,4 +1,89 @@
 'use strict';
 
-const minHeap = require('./min-heap') ;
+class PriorityQueue {
+  constructor(){
+    this._data = [];
+  }
 
+  _getParentIndex(index) {
+    if (index === 0)
+      return null;
+
+    return Math.floor((index - 1) / 2);
+  }
+
+  _getLeftIndex(index) {
+    return (2 * index) + 1;
+  }
+
+  _getRightIndex(index) {
+    return (2 * index) + 2;
+  }
+
+  // lg n
+  enqueue(priorityObject) {
+    if (typeof priorityObject.priority !== 'number')
+      throw new TypeError('__ERROR__ Priority should be numeric');
+
+    this._data.push(priorityObject);
+    this._bubbleUp(this._data.length - 1);
+  }
+
+  _swapValues(indexA, indexB) {
+    let tempSwapValue = this._data[indexA];
+
+    this._data[indexA] = this._data[indexB];
+    this._data[indexB] = tempSwapValue;
+  }
+
+  _bubbleUp(index) {
+    if (this._getParentIndex(index) === null)
+      return;
+
+    let parentIndex = this._getParentIndex(index);
+
+    if (this._data[parentIndex].priority > this._data[index].priority) {
+      this._swapValues(parentIndex, index);
+      this._bubbleUp(parentIndex);
+    }
+  }
+
+  _bubbleDown(index) {
+    let minIndex = index;
+    let leftIndex = this._getLeftIndex(index);
+    let rightIndex = this._getRightIndex(index);
+
+    if (leftIndex <= this._data.length - 1) {
+      if (this._data[minIndex].priority > this._data[leftIndex].priority)
+        minIndex = leftIndex;
+    }
+    if (rightIndex <= this._data.length - 1) {
+      if (this._data[minIndex].priority > this._data[rightIndex].priority)
+        minIndex = rightIndex;
+    }
+
+    if (minIndex != index) {
+      this._swapValues(index, minIndex);
+      this._bubbleDown(minIndex);
+    }
+  }
+
+  dequeue() {
+    if (this._data.length <= 0)
+      return null;
+
+    let firstPriority = this._data[0].value;
+    let lastValue = this._data.pop();
+    this._data[0] = lastValue;
+    this._bubbleDown(0);
+    return firstPriority;
+  }
+
+  peek() {
+    if (this._data.length <= 0)
+      return null;
+
+    return this._data[0];
+  }
+};
+module.exports = PriorityQueue;
